4a24eb079716474ef563034f38b8715df16820fd
Percy Mittagadapala <willi.percy3@gmail.com>
2021-06-17 19:44:55 -0400

    2-JUN-Lab

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsItALeapYear(int Y)
+        {
+            if (((Y % 4 == 0) && (Y % 100 != 0))|| (Y % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
